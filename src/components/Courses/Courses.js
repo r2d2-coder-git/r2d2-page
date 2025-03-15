05103476c6c0661432974144a06ec44a73e2a756
Arturo Lorenzo <arturo.lorenzo.h@hotmail.com>
2025-03-15 19:39:26 +0100

    add new course

--- src/components/Courses/Courses.js
+++ src/components/Courses/Courses.js
@@ -1,20 +1,34 @@
 import React from 'react';
 import { Container, Row, Col, Button } from 'react-bootstrap';
-import './Courses.css'; 
+import './Courses.css';
 import '../../css/shared_styles.css'
 import pythonImage from '../../images/python.svg'
 import reportImage from '../../images/report.svg'
 import awsImage from '../../images/aws.svg'
 
+const renderNewLabel = (course) => {
+  if (course.isNew) {
+    return (
+      <p className='text-danger' style={{ marginTop: '10px', marginBottom: '10px' }}>
+        <b><em>¡Recién lanzado!</em></b>
+      </p>
+    );
+  }
+  return null; // No renderiza nada si no es nuevo
+}
+
 const Courses = () => {
   const courses = [
-    { title: 'Python desde cero', instructor: 'Arturo Lorenzo', imageUrl: pythonImage, url: 'https://www.udemy.com/course/python-desde-cero-hasta-nivel-experto/?referralCode=8F7A5E56D33ACC40F277', available: true },
-    { title: 'Análisis de datos con Python', instructor: 'Arturo Lorenzo', imageUrl: reportImage, url: 'url2', available: false }
+    { title: 'Python desde cero', instructor: 'Arturo Lorenzo', imageUrl: pythonImage, url: 'https://www.udemy.com/course/python-desde-cero-hasta-nivel-experto/?referralCode=8F7A5E56D33ACC40F277', available: true, isNew: false },
+    { title: 'Análisis de datos con Python', instructor: 'Arturo Lorenzo', imageUrl: reportImage, url: 'https://www.udemy.com/course/analisis-de-datos-con-python-total-en-7-dias/?couponCode=9E464FA0CB4E876403A0', available: true, isNew: true }
   ];
 
   return (
     <Container className="satoshi-medium main-container" fluid>
-      <h2 className='text-center' style={{ marginBottom: '30px' }}>Aprende con mis cursos</h2>
+      <h2 className='text-center' style={{ marginBottom: '10px' }}>Aprende con mis cursos</h2>
+      <p className="fs-4 fw-bold text-danger">
+        <b><em>¡Nuevo curso disponible!🔥</em></b>
+      </p>
       <Row className='center-row'>
         {courses.map((course, index) => (
           <Col key={index} xs={12} sm={6} md={4} lg={3} className='text-center'>
@@ -23,10 +37,11 @@ const Courses = () => {
               <h3>{course.title}</h3>
               <p>Instructor: {course.instructor}</p>
               {course.available ? (
-                <Button className = 'button-generic' variant="primary" href={course.url} target="_blank" style={{ marginTop: '10px', marginBottom: '10px' }}>Ver más</Button>
+                <Button className='button-generic' variant="primary" href={course.url} target="_blank" style={{ marginTop: '10px', marginBottom: '10px' }}>Ver más</Button>
               ) : (
                 <p style={{ marginTop: '10px', marginBottom: '10px' }}><b><em>Próximamente</em></b></p>
               )}
+              {renderNewLabel(course)}
             </div>
           </Col>
         ))}
