81bd3bdc4ad04775d0b3e0e2af4143c0a23949b2
Arturo Lorenzo <35840182+r2d2-coder-git@users.noreply.github.com>
2025-05-19 17:06:26 +0200

    Update CollabLinks.js

--- src/components/CollabLinks/CollabLinks.js
+++ src/components/CollabLinks/CollabLinks.js
@@ -6,9 +6,7 @@ import temu from '../../images/temu.png'
 
 const CollabLinks = () => {
   const links = [
-    { image: temu, title: 'TECLADO TEMU (Código DTO: dwf7748)', url: 'https://www.temu.com/es/teclado-mecanico-ajazz-ak820pro-75-con-pantalla-tft-intercambiable-en-caliente-triple--inalambrico-2-4g-cableado-retroiluminacion-rgb-led-compatible-para-juegos-y--para-windows--bateria-de-4000mah-g-601099710772940.html' },
-    { image: temu, title: 'PANTALLA PORTÁTIL TEMU (Código DTO: dwf7748)', url: 'https://www.temu.com/es/monitor-portatil-con-pantalla---de-15-6-fhd-1080p--lcd-pantalla-ips-de-1920x1080---de-3-ms-75-srgb-mate--usb-compatible-con-computadora-portatil-ps5-4-pc--g-601099733728761.html' }
-  ];
+    { image: temu, title: 'TEMU (Código DTO: dwf7748)', url: 'https://app.temu.com/k/esmzh8i3hj8' }  ];
 
   return (
     <Container className='satoshi-medium main-container'>
