72b10d471e67e236b2515c1c83f37fc70a82cb17
Arturo Lorenzo <arturo.lorenzo.h@hotmail.com>
2026-03-13 20:21:56 +0100

    upload

--- src/components/CollabLinks/CollabLinks.js
+++ src/components/CollabLinks/CollabLinks.js
@@ -2,15 +2,17 @@ import React from 'react';
 import { Container, Row, Col, Button } from 'react-bootstrap';
 import './CollabLinks.css';
 import '../../css/shared_styles.css';
+import blink from '../../images/blink.png'
+import atoms from '../../images/atoms.png'
+import skywork from '../../images/skywork.png'
 import emergent from '../../images/emergent.png'
-import readdy from '../../images/readdy.png'
-import verdent from '../../images/verdent.svg'
 
 const CollabLinks = () => {
   const links = [
-    { image: readdy, title: 'READDY AI', url: 'https://bit.ly/Readdy01_ArturoLorenzo' } ,
-    { image: verdent, title: 'VERDENT AI', url: 'https://www.verdent.ai/es?id=700036&utm_source=ig&utm_medium=social&utm_content=link_in_bio&fbclid=PAZXh0bgNhZW0CMTEAc3J0YwZhcHBfaWQMMjU2MjgxMDQwNTU4AAGnpFLTqV45CgtCNuscIjY9XiDqesDL0KwPgI57sZzPfzyjZt05Ylmf_ictdRY_aem_okP-LKZLhTEoiRbwDOMITw' } ,
-    { image: emergent, title: 'EMERGENT: AGENTE DE AI', url: 'https://app.emergent.sh/?via=r2d2tech' } ,
+     { image: blink, title: 'BLINK', url: 'https://blink.new/' } ,
+    { image: atoms, title: 'ATOMS', url: 'https://atoms.dev/?utm_source=taidong&utm_medium=r2d2.coder&utm_content=instagram&utm_source=ig&utm_medium=social&utm_content=link_in_bio&fbclid=PAZXh0bgNhZW0CMTEAc3J0YwZhcHBfaWQMMjU2MjgxMDQwNTU4AAGndZzBSxfskNIZSn7MMQjiNUSZFd0If-JImUWVsD8CrF65gyoLSAFhy6I7AVs_aem_8Y3RNN6lx92-NyC3OkS-kg' } ,
+    { image: skywork, title: 'SKYWORK', url: 'https://skywork.ai/?referrer=r2d2coder&shortlink_id=2032034977147248640&utm_source=ins' } ,
+    { image: emergent, title: 'EMERGENT', url: 'https://app.emergent.sh/?via=r2d2tech' } ,
    ];
 
   return (
